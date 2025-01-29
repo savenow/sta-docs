6ae7bd3c60df3924d6b26b14ef6d578cb846da6b
Benedikt Schwab <benedikt.schwab@gmail.com>
2025-01-29 16:58:12 +0100

    updated links

--- src/conf.py
+++ src/conf.py
@@ -107,7 +107,7 @@ html_favicon = 'favicon.png'
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-html_baseurl = "https://sta-rt.savenow.de/"
+html_baseurl = "https://savenow.github.io/sta-docs/"
 
 html_theme_options = {
     #'analytics_id': 'UA-XXXXXXX-1',  #  Provided by Google in your dashboard
